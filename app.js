f0971e0ef45de8ea98f7a1add3264b383adc6524
Carinaps <carinaps1989@gmail.com>
2026-06-26 23:25:42 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById("novoPPC").addEventListener("click", () => {
+    alert("Novo PPC");
+});
+
+document.getElementById("abrirPPC").addEventListener("click", () => {
+    alert("Abrir PPC");
+});
