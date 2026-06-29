3f272765091476a1986c32ff17078da9ad273fd9
Carinaps <carinaps1989@gmail.com>
2026-06-29 20:43:08 -0300

    Implement PPC management module with basic functions

--- js/ppc.js
+++ js/ppc.js
@@ -1,1 +1,87 @@
+/**
+ * ==========================================
+ * SIGPPC
+ * Módulo de Gerenciamento de PPCs
+ * ==========================================
+ */
 
+const PPC = (() => {
+
+    let lista = [];
+
+    function inicializar() {
+
+        Utils.log("Módulo PPC inicializado.");
+
+        carregar();
+
+    }
+
+    function carregar() {
+
+        lista = [];
+
+    }
+
+    function listar() {
+
+        return Utils.deepClone(lista);
+
+    }
+
+    function adicionar(ppc) {
+
+        if (!ppc) return false;
+
+        lista.push(ppc);
+
+        salvar();
+
+        return true;
+
+    }
+
+    function remover(id) {
+
+        lista = lista.filter(item => item.id !== id);
+
+        salvar();
+
+    }
+
+    function buscar(id) {
+
+        return lista.find(item => item.id === id);
+
+    }
+
+    function salvar() {
+
+        // Integração com storage.js
+        // será realizada na próxima sprint.
+
+    }
+
+    function total() {
+
+        return lista.length;
+
+    }
+
+    return {
+
+        inicializar,
+
+        listar,
+
+        adicionar,
+
+        remover,
+
+        buscar,
+
+        total
+
+    };
+
+})();
